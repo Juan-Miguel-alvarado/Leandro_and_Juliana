ab4b21b2c0b7d12af5eb69ddfb2005df23cc473a
Juan-Miguel-alvarado <juanchomigue2012@gmail.com>
2024-06-16 17:47:32 -0500

    Header + blur effect and js code + sections class

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+/*==== HEADER ====*/
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header');
+
+    const onScroll = () => {
+        if (window.scrollY > 0) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    };
+
+    window.addEventListener('scroll', onScroll);
+    
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('animationend', (event) => {
+        if (event.animationName === 'moveOut') {
+            document.body.classList.add('animation-complete');
+            header.classList.add('visible');
+        }
+    });
+});
